5afe44c590afc3e5235402ce80f71acfd444a792
Dylanlummer <dylanmplummer@gmail.com>
2021-05-16 13:00:17 -0400

    -Add option to compare performance with or without GO attributes when plotting performance

--- embed_nodes.py
+++ embed_nodes.py
@@ -9,7 +9,6 @@ from itertools import combinations
 from scipy.sparse import coo_matrix
 from scipy.spatial.distance import pdist
 from sklearn.manifold import TSNE
-from karateclub import DANMF
 from karateclub import ASNE, SINE, BANE, AE, MUSAE, TENE, TADW, FSCNMF
 
 from cluster_core_utils import *
@@ -18,11 +17,13 @@ from cluster_core_utils import *
 dset_file = 'gavin'
 dataset = dset_file
 n_exp = 1
-use_go = True
+use_go = False
 if use_go:
     dataset += '_GO'
 out_dir = 'plots/%s' % dataset
+results_dir = 'complex_results'
 os.makedirs(out_dir, exist_ok=True)
+os.makedirs(results_dir, exist_ok=True)
 
 A = np.loadtxt('matrix/Attribute_%s.txt' % dset_file)
 M = np.loadtxt('matrix/Network_%s.txt' % dset_file)
@@ -56,64 +57,65 @@ plt.xlabel("rank")
 plt.savefig(os.path.join(out_dir, 'degree_distribution.png'))
 plt.close()
 
-plt.matshow(nx.normalized_laplacian_matrix(G).A, cmap='nipy_spectral')
+plt.matshow(nx.normalized_laplacian_matrix(G).A, cmap='prism')
 plt.savefig(os.path.join(out_dir, 'laplacian_norm.png'))
 plt.close()
-# get attribute matrix and convert to sparse format for attributed embedding methods
-if use_go:
-    attr_m = coo_matrix(A)
-else:
-    attr_m = coo_matrix(np.zeros_like(A))
 
 # dict for storing results (will be converted to DataFrame)
-metrics = {'method': [], 'precision': [], 'recall': [], 'F1': [], 'Sn': [], 'PPV': [], 'Acc': []}
-for method_name, method, community_detector in zip(['TENE', 'TADW', 'FSCNMF', 'ASNE', 'DANMF', 'SINE', 'BANE', 'AE', 'MUSAE'],  # the name of the embedding/community detection method
-                                                   [TENE, TADW, FSCNMF, ASNE, DANMF, SINE, BANE, AE, MUSAE],  # the model class
-                                                   [False, False, False, False, True, False, False, False, False]):  # whether the model can predict community membership
+metrics = {'method': [], 'precision': [], 'recall': [], 'F1': [], 'Sn': [], 'PPV': [], 'Acc': [], 'use_GO': []}
+for method_name, method in zip(['TENE', 'TADW', 'FSCNMF', 'ASNE', 'SINE', 'BANE', 'AE', 'MUSAE'],  # the name of the embedding/community detection method
+                                [TENE, TADW, FSCNMF, ASNE, SINE, BANE, AE, MUSAE]):  # the model class
     for exp_i in range(n_exp):
-        print('Testing', method_name, exp_i)
-        os.makedirs(os.path.join(out_dir, method_name), exist_ok=True)
-        model = method()
-        model.allow_disjoint = True
-        community_df = pd.DataFrame()
-        try:
-            if community_detector:  # model can predict community membership
-                model.fit(G)
-                z = model.get_embedding()
-            else:  # otherwise run density-based clustering in embedding space
+        for use_go in [True, False]:
+            if use_go:
+                attr_m = coo_matrix(A)
+                dataset = dset_file + '_GO'
+            else:
+                attr_m = coo_matrix(np.ones_like(A))
+                dataset = dset_file
+            out_dir = 'plots/%s' % dataset
+            os.makedirs(out_dir, exist_ok=True)
+
+            print('Testing', method_name, exp_i, use_go)
+            os.makedirs(os.path.join(out_dir, method_name), exist_ok=True)
+            model = method()
+            model.allow_disjoint = True
+            community_df = pd.DataFrame()
+            try:
                 model.fit(G, attr_m)
                 z = model.get_embedding()
-        except AssertionError as e:
-            print(e)
-            continue
-        dist = pdist(z, metric='cosine')  # compute pairwise similarity between every pair of nodes
-        out_arr = np.vstack([comb, dist]).T
-        out_df = pd.DataFrame(data=out_arr, columns=['p1', 'p2', 'edge', 'sim'])
-        out_df = out_df.loc[out_df['edge'], ['p1', 'p2', 'sim']]  # filter out rows where proteins did not share an edge
-        out_df.to_csv(os.path.join(out_dir, method_name, '%s_sim.txt' % dataset), sep=' ', header=False, index=False)
+            except AssertionError as e:
+                print(e)
+                continue
+            dist = pdist(z, metric='cosine')  # compute pairwise similarity between every pair of nodes
+            out_arr = np.vstack([comb, dist]).T
+            out_df = pd.DataFrame(data=out_arr, columns=['p1', 'p2', 'edge', 'sim'])
+            out_df = out_df.loc[out_df['edge'], ['p1', 'p2', 'sim']]  # filter out rows where proteins did not share an edge
+            out_df.to_csv(os.path.join(out_dir, method_name, '%s_sim.txt' % dataset), sep=' ', header=False, index=False)
 
-        if exp_i == 0:
-            results_df = pd.DataFrame.copy(protein_df, deep=True)
-            columns_names = ['protein'] + list(np.arange(0, len(results_df.columns) - 1))
-            results_df.columns = [str(v) for v in columns_names]
-            z_tsne = TSNE(2).fit_transform(z)
-            results_df['tsne_1'] = z_tsne[..., 0]
-            results_df['tsne_2'] = z_tsne[..., 1]
-            results_df['degree'] = degrees
-            results_df.to_csv(os.path.join(out_dir, method_name, '%s_embeddings.csv' % dataset))
+            if exp_i == 0:
+                results_df = pd.DataFrame.copy(protein_df, deep=True)
+                columns_names = ['protein'] + list(np.arange(0, len(results_df.columns) - 1))
+                results_df.columns = [str(v) for v in columns_names]
+                z_tsne = TSNE(2).fit_transform(z)
+                results_df['tsne_1'] = z_tsne[..., 0]
+                results_df['tsne_2'] = z_tsne[..., 1]
+                results_df['degree'] = degrees
+                results_df.to_csv(os.path.join(out_dir, method_name, '%s_embeddings.csv' % dataset))
 
-        cluster(dataset, dset_file, method_name)
-        precision, recall, F1, Sn, PPV, Acc = eval(dataset)
-        metrics['method'] += [method_name]
-        metrics['precision'] += [precision]
-        metrics['recall'] += [recall]
-        metrics['F1'] += [F1]
-        metrics['Sn'] += [Sn]
-        metrics['PPV'] += [PPV]
-        metrics['Acc'] += [Acc]
+            cluster(dataset, dset_file, method_name)
+            precision, recall, F1, Sn, PPV, Acc = eval(dataset)
+            metrics['method'] += [method_name]
+            metrics['use_GO'] += [use_go]
+            metrics['precision'] += [precision]
+            metrics['recall'] += [recall]
+            metrics['F1'] += [F1]
+            metrics['Sn'] += [Sn]
+            metrics['PPV'] += [PPV]
+            metrics['Acc'] += [Acc]
 
-df = pd.DataFrame.from_dict(metrics)
-df.to_csv('complex_identification_results.csv')
+    df = pd.DataFrame.from_dict(metrics)
+    df.to_csv(os.path.join(results_dir, 'complex_identification_results_%s.csv' % dataset))
 
 methods = df['method'].unique()
 method_map = {}
@@ -123,8 +125,7 @@ df['hue'] = df['method'].apply(lambda m: method_map[m])
 print(df)
 
 for key in ['precision', 'recall', 'F1', 'Sn', 'PPV', 'Acc']:
-    g = sns.barplot(data=df, x='method', y=key, order=df.sort_values(by=key).method)
-    g.legend_.remove()
+    g = sns.barplot(data=df, x='method', y=key, order=df.sort_values(by=key).method, hue='use_GO')
     plt.ylim(0, 1)
     plt.savefig(os.path.join(out_dir, '%s.png' % (key)))
     plt.close()
--- plot_identification_results.py
+++ plot_identification_results.py
@@ -7,9 +7,10 @@ import seaborn as sns
 plt.style.use('seaborn')
 
 out_dir = 'plots'
+dataset = 'gavin'
 os.makedirs(out_dir, exist_ok=True)
 
-df = pd.read_csv('complex_identification_results.csv')
+df = pd.read_csv('complex_results/complex_identification_results_%s.csv' % dataset)
 methods = df['method'].unique()
 method_map = {}
 for i, method in enumerate(methods):
@@ -18,8 +19,8 @@ df['hue'] = df['method'].apply(lambda m: method_map[m])
 print(df)
 
 for key in ['precision', 'recall', 'F1', 'Sn', 'PPV', 'Acc']:
-    g = sns.barplot(data=df, x='method', y=key, order=df.drop_duplicates(subset='method').sort_values(by=key).method)
+    g = sns.barplot(data=df, x='method', y=key, order=df.drop_duplicates(subset='method').sort_values(by=key).method, hue='use_GO')
     #g.legend_.remove()
     plt.ylim(0, 1)
-    plt.savefig(os.path.join(out_dir, '%s.png' % (key)))
+    plt.savefig(os.path.join(out_dir, '%s_%s.png' % (dataset, key)))
     plt.close()
